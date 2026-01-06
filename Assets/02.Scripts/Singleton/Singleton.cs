99ab0dae3c878336144e5b369b7cc123cde54c69
권정민 <kopoma1235@gmail.com>
2026-01-06 13:19:33 +0900

    Generic Singleton Make

--- Assets/02.Scripts/Singleton/Singleton.cs
+++ Assets/02.Scripts/Singleton/Singleton.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Singleton : MonoBehaviour
+/// <summary>
+/// 공통 Singleton 베이스.
+/// - 씬에 배치된 인스턴스를 사용
+/// - 중복 생성 방지
+/// - 씬전환 대비 DDOL
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T _instance;
+
+    public static T Instance
     {
-        
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if(_instance == null)
+                {
+                    Debug.LogError($"[Singleton] {typeof(T).Name} 인스턴스를 씬에서 찾을 수 없음");
+                }
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Awake()
     {
-        
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
+        DontDestroyOnLoad(gameObject);
     }
 }
